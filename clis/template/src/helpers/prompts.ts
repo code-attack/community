72af3d96046973d641c20075bd2bcb9c4d64806d
Josanghyeon <05lazy.dev@gmail.com>
2023-06-16 10:06:57 +0900

    생성 prompts 수정

--- clis/template/src/helpers/prompts.ts
+++ clis/template/src/helpers/prompts.ts
@@ -1,52 +1,53 @@
-import path from 'path';
-import prompt from 'prompts';
+import path from "path";
+import prompt from "prompts";
 
-import { validateNpmName } from './pkg-name-validate';
+import { validateNpmName } from "./pkg-name-validate";
 
 const kind = async () => {
-    const template = await prompt({
-        type: 'select',
-        name: 'value',
-        message: '무슨 템플릿을 복사하시겠어요?',
-        choices: [
-            { title: 'next', value: 'next-app' },
-            { title: 'express', value: 'express-app' },
-        ],
-    });
+  const template = await prompt({
+    type: "select",
+    name: "value",
+    message: "무슨 템플릿을 복사하시겠어요?",
+    choices: [
+      { title: "next", value: "next-app" },
+      { title: "express", value: "express-app" },
+      { title: "react", value: "react-app" },
+    ],
+  });
 
-    return { template: template.value };
+  return { template: template.value };
 };
 
 const dir = async () => {
-    const dir = await prompt({
-        type: 'select',
-        name: 'value',
-        message: '무슨 경로에 만드실껀가요?',
-        choices: [
-            { title: 'pacakge', value: 'packages' },
-            { title: 'service', value: 'services' },
-        ],
-    });
+  const dir = await prompt({
+    type: "select",
+    name: "value",
+    message: "무슨 경로에 만드실껀가요?",
+    choices: [
+      { title: "pacakge", value: "packages" },
+      { title: "service", value: "services" },
+    ],
+  });
 
-    return { dir: dir.value };
+  return { dir: dir.value };
 };
 
 const packageName = async () => {
-    const packageName = await prompt({
-        type: 'text',
-        name: 'value',
-        message: '패키지 이름은 무엇인가요?',
-        validate: (name) => {
-            const validation = validateNpmName(path.basename(path.resolve(name)));
-            if (validation.valid) {
-                return true;
-            }
+  const packageName = await prompt({
+    type: "text",
+    name: "value",
+    message: "패키지 이름은 무엇인가요?",
+    validate: (name) => {
+      const validation = validateNpmName(path.basename(path.resolve(name)));
+      if (validation.valid) {
+        return true;
+      }
 
-            return 'Invalid project name: ' + validation.problems![0];
-        },
-    });
+      return "Invalid project name: " + validation.problems![0];
+    },
+  });
 
-    return { packageName: packageName.value.trim() };
+  return { packageName: packageName.value.trim() };
 };
 
 type Prompts = Record<string, () => Promise<Record<string, string>>>;
