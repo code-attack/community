1877782fe3cfe2b80c451774f1f06ea65bf6b525
Josanghyeon <05lazy.dev@gmail.com>
2023-06-16 10:28:22 +0900

    next template

--- clis/template/_templates/express-app/src/loaders/index.ts
+++ clis/template/_templates/express-app/src/loaders/index.ts
@@ -1,17 +1,18 @@
-import { Application, json } from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import { router } from '../routes';
+import cors from "cors";
+import morgan from "morgan";
+import { Application, json } from "express";
+
+import { router } from "../apis/routes";
 
 export const loader = (app: Application) => {
-    app.use(json());
+  app.use(json());
 
-    app.use(morgan('dev'));
-    app.use(cors());
+  app.use(morgan("dev"));
+  app.use(cors());
 
-    app.use('/', router);
+  app.use("/", router);
 
-    app.get('/status', (req, res) => {
-        return res.json({});
-    });
+  app.get("/status", (req, res) => {
+    return res.json({});
+  });
 };
--- clis/template/_templates/express-app/src/routes/auth/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { signIn, signUp } from './controller';
-
-export const router = Router();
-
-router.post('sign-in', signIn);
-router.post('sign-up', signUp);
