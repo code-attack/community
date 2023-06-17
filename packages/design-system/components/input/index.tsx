e16df79e8e5a53095a678da7f7ef30d4a3a61b5e
ipiyou <tao710803@gmail.com>
2023-06-18 01:38:21 +0900

    outline 퍼블리싱

--- packages/design-system/components/input/index.tsx
+++ packages/design-system/components/input/index.tsx
@@ -2,8 +2,8 @@ import { Blocking, Search, Error } from "../../assets/svg";
 
 interface PropsType {
   kind?: "fill" | "outline";
-  onSearch: () => void;
-  onClear: () => void;
+  onSearch?: () => void;
+  onClear?: () => void;
   value: string;
   placeholder?: string;
   disabled?: boolean;
@@ -16,52 +16,75 @@ const disabledStyleObject = {
   outline: "",
 } as const;
 
+const KindStyleObject = {
+  fill: {
+    wrapper: "rounded-[4px_4px_0_0] bg-[#E7E0EC] border-b-2",
+    active:
+      "focus-within:border-b-4 focus-within:pt-0.5 hover:bg-[rgba(28,27,31,0.08)]",
+    label:
+      "[&>label]:focus-within:top-1 [&>label]:transition-all [&>input]:focus-within:mt-4",
+  },
+  outline: {
+    wrapper: "rounded bg-white border-[1px]",
+    active: "focus-within:border-2 focus-within:px-[11px]",
+    label:
+      "focus-within:relative [&>label]:focus-within:-top-2 [&>label]:left-0  [&>label]:bg-white [&>label]:duration-700",
+  },
+};
+
 export const Input = ({
+  onSearch,
+  onClear,
   value,
   placeholder,
   disabled,
-  errorMsg = "asga",
+  errorMsg,
   kind = "fill",
   supportMsg,
 }: PropsType) => {
-  const isError = errorMsg;
   const isValuePlaceholderNone = !(value || placeholder);
 
   const addDisableStyle = (style?: string | boolean) =>
     disabled ? disabledStyleObject[kind] : style;
 
+  const { wrapper, active, label } = KindStyleObject[kind];
+
+  const outlineLabel =
+    kind === "outline" && onSearch && `[&>label]:focus-within:-left-[32px]`;
   return (
     <div>
       <div
-        className={`h-14 w-[210px] transition-all flex items-center px-3 gap-3 box-border rounded-[4px_4px_0_0] bg-[#E7E0EC] border-b-[2px] [&>svg]:shrink-0 ${addDisableStyle(
+        className={`h-14  w-[210px] relative transition-all flex items-center px-3 gap-3 box-border ${wrapper} [&>svg]:shrink-0 ${addDisableStyle(
           `${
             errorMsg && "[&_label]:text-[#B3261E] border-[#B3261E]"
-          } border-[#79747E] focus-within:border-b-4 focus-within:border-[#6750A4] focus-within:pt-0.5 b hover:bg-[rgba(28,27,31,0.08)]`
+          } border-[#79747E] ${active}  focus-within:border-[#6750A4]`
         )}  `}
       >
-        <Search />
+        {onSearch && <Search className="relative z-20" />}
         <div
-          className={`relative h-full flex flex-col justify-center [&>label]:focus-within:text-[#6750A4] ${
+          className={`h-full flex flex-col justify-center [&>label]:focus-within:text-[#6750A4] ${
             isValuePlaceholderNone &&
-            " [&>label]:focus-within:text-xs [&>label]:focus-within:top-1 [&>input]:h-full [&>input]:focus-within:h-auto [&>input]:focus-within:mt-4 [&>input]:opacity-0 [&>input]:focus-within:opacity-100"
+            `${label} ${outlineLabel} relative [&>label]:focus-within:text-xs [&>input]:h-full [&>input]:focus-within:h-auto [&>input]:opacity-0 [&>input]:focus-within:opacity-100`
           }`}
         >
           <label
             className={`${
               isValuePlaceholderNone ? "text-base absolute top-4" : "text-xs"
-            } z-0 transition-all top-3  text-[#49454F] `}
+            } z-0  top-3 text-[#49454F] px-1`}
           >
             Label
           </label>
           {
             <input
-              className=" relative z-10 w-full min-w-0 text-base bg-transparent outline-none transition-all"
+              className={`${onSearch && " ml-1"} ${
+                onClear && " mr-1"
+              } relative z-10 w-full min-w-0 text-base bg-transparent outline-none transition-all`}
               value="Input"
             />
           }
         </div>
 
-        {errorMsg && !disabled ? <Error /> : <Blocking />}
+        {onClear && (errorMsg && !disabled ? <Error /> : <Blocking />)}
       </div>
       <div
         className={`text-xs mt-1 ml-4 ${addDisableStyle(
