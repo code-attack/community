418d1c0f679acea8f3b5d88c7d59b0731c7cbf22
ipiyou <tao710803@gmail.com>
2023-06-20 18:01:04 +0900

    주석 삭제

--- services/web/src/components/Text/constants.ts
+++ services/web/src/components/Text/constants.ts
@@ -1,15 +1,3 @@
-/* 
-  thin: 100,
-  extraLight: 200,
-  light: 300,
-  regular: 400,
-  medium: 500,
-  semiBold: 600,
-  bold: 700,
-  extraBold: 800,
-  black: 900,
-*/
-
 export const fontObject = {
   heading1: `font-semiBold text-[48px]`,
   heading2: `font-semiBold text-[36px]`,
