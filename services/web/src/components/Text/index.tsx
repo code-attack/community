b54b9f2ad5d259396765324f2309cd4942d407c3
ipiyou <tao710803@gmail.com>
2023-06-20 17:26:59 +0900

    as로 태그 변경

--- services/web/src/components/Text/index.tsx
+++ services/web/src/components/Text/index.tsx
@@ -1,12 +1,19 @@
-import { ReactNode } from "react";
+import { ReactNode, createElement } from "react";
 import { fontObject, keyOfFont } from "./constants";
 
 interface PropsType {
+  as?: "div" | "p" | "span" | "li";
   className?: string;
   size?: keyOfFont;
   children?: ReactNode;
 }
 
-export const Text = ({ className, size = "body3", children }: PropsType) => {
-  return <div className={`${className} ${fontObject[size]}`}>{children}</div>;
+export const Text = ({
+  as = "div",
+  className = "",
+  size = "body3",
+  children,
+}: PropsType) => {
+  const textClassName = `${className} ${fontObject[size]}`;
+  return createElement(as, { className: textClassName, children });
 };
