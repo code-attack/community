e2fbd9758e064b032f5cf62ef18ace0e81f82c86
ipiyou <tao710803@gmail.com>
2023-06-20 21:52:24 +0900

    input 틀 생성

--- services/web/src/assets/svg/index.ts
+++ services/web/src/assets/svg/index.ts
@@ -1,1 +1,10 @@
+export * from "./Open";
+export * from "./Close";
+
+export interface SvgPropsType {
+  onClick?: (e: React.MouseEvent<SVGElement, MouseEvent>) => void;
+  size?: number;
+  className?: string;
+}
+
 export * from "./Arrow";
--- services/web/src/components/input/index.tsx
+++ services/web/src/components/input/index.tsx
@@ -1,3 +1,37 @@
-export const Input = () => {
-  return <input className=" w-full h-14" />;
+"use client";
+
+import { Close, Open } from "@/assets/svg";
+import { ChangeEvent, useState } from "react";
+import { Text } from "../Text";
+
+interface PropsType {
+  value: string;
+  name: string;
+  placeholder: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+
+  type?: "text" | "password";
+
+  label?: string;
+  hint?: string[];
+  error?: boolean[];
+}
+
+export const Input = ({ type }: PropsType) => {
+  const [close, setClose] = useState<boolean>(false);
+  const reverseClose = () => setClose(!close);
+  return (
+    <div>
+      <Text.body3 className=" ml-1 mb-1">라벨입니다</Text.body3>
+      <div className="relative">
+        <input
+          className=" w-full h-12 outline-none border-[1px] box-border border-gray200 rounded-[4px]"
+          type={close ? type : "text"}
+        />
+        <div className="absolute top-3 right-4">
+          {close ? <Close /> : <Open />}
+        </div>
+      </div>
+    </div>
+  );
 };
