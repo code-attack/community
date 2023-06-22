39bd88d66f215a8356710330a7f2086b82c4d6af
ipiyou <tao710803@gmail.com>
2023-06-22 23:16:25 +0900

    프로필 작성 모달 생성

--- services/web/src/app/profile/write/page.tsx
+++ services/web/src/app/profile/write/page.tsx
@@ -10,9 +10,12 @@ import Image from "next/image";
 import { Footer } from "@/components/common/footer";
 import { Profile } from "@/components/profile";
 import { DateInput } from "@/components/common/date";
-import { useState } from "react";
+import { Dispatch, ReactNode, SetStateAction, useState } from "react";
 import { query } from "@/hooks";
 import { User } from "@package/api-types";
+import OutsideClickHandler from "react-outside-click-handler";
+
+type WriteType = "profile" | "basic" | "experience";
 
 export default () => {
   const { data } = query.user.getUser();
@@ -36,16 +39,24 @@ const Main = ({
 }: User.MyProfileRes) => {
   const [date, setDate] = useState<number | undefined>(undefined);
 
+  const [type, setType] = useState<WriteType>("profile");
+
+  const [modal, setModal] = useState<boolean>(false);
+  const openModal = (type: WriteType) => {
+    setType(type);
+    setModal(true);
+  };
+  const closeModal = () => modal && setModal(false);
   return (
     <main className="m-auto w-[1016px]">
       {/* <Profile.Patch /> */}
       <Link href={"/"}>
         <Button.Text Icon={<Svg.Arrow />} className="w-28 my-10">
-          홈으로
+          홈으로 {modal ? 1 : 2}
         </Button.Text>
       </Link>
       <Text.heading3>프로필 작성</Text.heading3>
-      <Profile.Block>
+      <Profile.Block onClick={() => openModal("profile")}>
         <Image
           src={profile_img || "/auth.png"}
           alt="profile"
@@ -55,25 +66,25 @@ const Main = ({
         />
         <Text.heading3>{name}</Text.heading3>
       </Profile.Block>
-      <Profile.Block>
+      <Profile.Block onClick={() => openModal("basic")}>
         <Text.heading3 className="mb-4">기본정보</Text.heading3>
         <div>
           <Text.title2 className="text-gray-400">자기소개</Text.title2>
-          <Text.body1>{introduce.content}</Text.body1>
+          <Text.body1>{introduce?.content}</Text.body1>
         </div>
         <div>
           <Text.title2 className="text-gray-400">태그</Text.title2>
-          <Text.body1>{introduce.content}</Text.body1>
+          <Text.body1>{introduce?.content}</Text.body1>
         </div>
         <div>
           <Text.title2 className="text-gray-400">기술스택</Text.title2>
-          <Text.body1>{introduce.content}</Text.body1>
+          <Text.body1>{introduce?.content}</Text.body1>
           <Input.Skill value={[]} placeholder="" onChange={() => {}} name="" />
         </div>
       </Profile.Block>
-      <Profile.Block>
+      <Profile.Block onClick={() => openModal("experience")}>
         <Text.heading3 className="mb-4">업무 경험</Text.heading3>
-        {workExperience.map((work) => (
+        {workExperience?.map((work) => (
           <div className="pl-12 relative pb-4">
             <div className="border-l-2 border-gray-100 h-[calc(100%-10px)] w-[2px] top-5 left-[7px] absolute" />
             <div>
@@ -88,6 +99,55 @@ const Main = ({
           </div>
         ))}
       </Profile.Block>
+      <OutsideClickHandler onOutsideClick={closeModal}>
+        <WriteModal modal={modal} type={type} state={""} setState={() => {}} />
+      </OutsideClickHandler>
     </main>
+  );
+};
+
+interface WriteModalProps {
+  modal: boolean;
+  type: WriteType;
+  children?: ReactNode;
+  state: any;
+  setState: Dispatch<SetStateAction<any>>;
+}
+
+const WriteModal = ({ modal, type, state, setState }: WriteModalProps) => {
+  return (
+    <div
+      className={`${
+        modal ? "right-10" : "-right-[540px]"
+      } top-0 z-50 box-border my-10  h-[90%] w-[500px] py-10 px-12  fixed transition-all  bg-white rounded-md shadow-2xl`}
+    >
+      {
+        {
+          profile: (
+            <div className="flex flex-col gap-4">
+              <Input.Basic label="테스트" value="" onChange={() => {}} />
+              <DateInput
+                label="테스트"
+                value=""
+                onSubmitAtInput={() => {}}
+                name=""
+              />
+            </div>
+          ),
+          basic: (
+            <div className="flex flex-col gap-4">
+              <Input.Basic label="테스트" value="" onChange={() => {}} />
+              <Input.Basic label="테스트" value="" onChange={() => {}} />
+            </div>
+          ),
+          experience: (
+            <div className="flex flex-col gap-4">
+              <Input.Basic label="테스트" value="" onChange={() => {}} />
+              <Input.Basic label="테스트" value="" onChange={() => {}} />
+            </div>
+          ),
+        }[type]
+      }
+    </div>
   );
 };
--- services/web/src/components/profile/Block.tsx
+++ services/web/src/components/profile/Block.tsx
@@ -5,13 +5,17 @@ import { Icon } from "../common/button/Icon";
 interface Props {
   children: ReactNode;
   className?: string;
+  onClick?: (e: React.MouseEvent<SVGElement, MouseEvent>) => void;
 }
 
-export const Block = ({ children, className = "" }: Props) => {
+export const Block = ({ children, className = "", onClick }: Props) => {
   return (
     <div className={`bg-white rounded-2xl p-12 relative mt-6 ${className}`}>
       {children}
-      <Icon className="right-12 top-12 absolute" Icon={<Svg.Write />} />
+      <Icon
+        className="right-12 top-12 absolute"
+        Icon={<Svg.Write onClick={onClick} />}
+      />
     </div>
   );
 };
